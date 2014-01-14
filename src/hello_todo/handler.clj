a6e5c5a1ef3bf3bb2b738856203279a9e4a1ba14
FScoward <endlick1989@yahoo.co.jp>
2014-01-14 23:36:39 +0900

    TODOリストを溜め込む処理を作成。溜め込んだTODOリストを表示する処理を作成。

--- src/hello_todo/handler.clj
+++ src/hello_todo/handler.clj
@@ -6,8 +6,38 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+; TODO
+(defrecord Todo [no todo])
+
+; TODOリストを溜め込む
+(def todolist (ref ()))
+(defn add-todolist [item]
+  (dosync (alter todolist conj item)))
+
+; カウント用関数
+(def counter (ref 0))
+(defn next-counter [] (dosync (alter counter inc)))
+
+; todolist表示
+(defn display-todolist []
+  (map #(str "No: " (:no %) "   " "Item: " (:todo %) "</br>")(deref todolist)))
+
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [& {item :todo}]
+       (html5 [:body
+               (form-to [:GET "/"]
+                        (label "todo" "todo")
+                        (text-field "todo")
+                        [:br]
+                        (submit-button "submit")
+                        [:br]
+                        (if-not (nil? item)
+                          (do
+                            (add-todolist (Todo. (next-counter) item))
+                            (display-todolist))
+                          "no item"
+                          ))]))
   (route/resources "/")
   (route/not-found "Not Found"))
 
